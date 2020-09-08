f4b3fb388bd886e7211bdc404231658c2efc4e12
Matthew Gleich <matthewgleich@gmail.com>
2020-09-08 13:12:51 -0400

    ✨ Add init command
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- cmd/init.go
+++ cmd/init.go
@@ -1,8 +1,15 @@
 package cmd
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/Matt-Gleich/texsch/pkg/commands/configure"
+	"github.com/Matt-Gleich/texsch/pkg/commands/init"
 	"github.com/Matt-Gleich/texsch/pkg/commands/setroot"
+	"github.com/Matt-Gleich/texsch/pkg/configuration"
 	"github.com/Matt-Gleich/texsch/pkg/location"
+	"github.com/Matt-Gleich/texsch/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +19,15 @@ var initCmd = &cobra.Command{
 	Short: "Initialize a repo with texsch",
 	Run: func(cmd *cobra.Command, args []string) {
 		setroot.Set(location.GlobalConfigPath)
+		configure.Write(
+			configure.General(),
+			configure.Classes(),
+			configure.Templates(),
+		)
+		fmt.Println()
+		utils.WriteFileSafely(`.gitignore`, []byte(init.GitIgnore), true, true)
+		generalConfig := configuration.GetGeneral()
+		utils.WriteFileSafely(`LICENSE`, []byte(strings.ReplaceAll(strings.ReplaceAll(init.LICENSE, "CURRENT_YEAR", generalConfig.Year), "FULL_NAME", generalConfig.Full_Name)), true, true)
 	},
 }
 
--- pkg/commands/init/files.go
+++ pkg/commands/init/files.go
@@ -1,5 +1,215 @@
 package init
 
-func createFile(name string, content string) {
+const GitIgnore = `# Miscellaneous
+*.class
+*.log
+*.pyc
+*.swp
+.DS_Store
+.atom/
+.buildlog/
+.history
+.svn/
 
-}
+# IntelliJ related
+*.iml
+*.ipr
+*.iws
+.idea/
+
+# The .vscode folder contains launch configuration and tasks you configure in
+# VS Code which you may wish to be included in version control, so this line
+# is commented out by default.
+#.vscode/
+
+# Flutter/Dart/Pub related
+**/doc/api/
+.dart_tool/
+.flutter-plugins
+.flutter-plugins-dependencies
+.packages
+.pub-cache/
+.pub/
+/build/
+
+# Web related
+lib/generated_plugin_registrant.dart
+
+# Symbolication related
+app.*.symbols
+
+# Obfuscation related
+app.*.map.json
+
+# Exceptions to above rules.
+!/packages/flutter_tools/test/data/dart_dependencies_test/**/.packages
+
+# Automation Script Private files
+google-calendar.json
+
+## Core latex/pdflatex auxiliary files:
+*.aux
+*.lof
+*.log
+*.lot
+*.fls
+*.out
+*.toc
+
+## Intermediate documents:
+*.dvi
+*-converted-to.*
+# these rules might exclude image files for figures etc.
+# *.ps
+# *.eps
+# *.pdf
+
+## Bibliography auxiliary files (bibtex/biblatex/biber):
+*.bbl
+*.bcf
+*.blg
+*-blx.aux
+*-blx.bib
+*.brf
+*.run.xml
+
+## Build tool auxiliary files:
+*.fdb_latexmk
+*.synctex
+*.synctex.gz
+*.synctex.gz(busy)
+*.pdfsync
+
+## Auxiliary and intermediate files from other packages:
+
+
+# algorithms
+*.alg
+*.loa
+
+# achemso
+acs-*.bib
+
+# amsthm
+*.thm
+
+# beamer
+*.nav
+*.snm
+*.vrb
+
+#(e)ledmac/(e)ledpar
+*.end
+*.[1-9]
+*.[1-9][0-9]
+*.[1-9][0-9][0-9]
+*.[1-9]R
+*.[1-9][0-9]R
+*.[1-9][0-9][0-9]R
+*.eledsec[1-9]
+*.eledsec[1-9]R
+*.eledsec[1-9][0-9]
+*.eledsec[1-9][0-9]R
+*.eledsec[1-9][0-9][0-9]
+*.eledsec[1-9][0-9][0-9]R
+
+# glossaries
+*.acn
+*.acr
+*.glg
+*.glo
+*.gls
+
+# gnuplottex
+*-gnuplottex-*
+
+# hyperref
+*.brf
+
+# knitr
+*-concordance.tex
+*.tikz
+*-tikzDictionary
+
+# listings
+*.lol
+
+# makeidx
+*.idx
+*.ilg
+*.ind
+*.ist
+
+# minitoc
+*.maf
+*.mtc
+*.mtc[0-9]
+*.mtc[1-9][0-9]
+
+# minted
+_minted*
+*.pyg
+
+# morewrites
+*.mw
+
+# mylatexformat
+*.fmt
+
+# nomencl
+*.nlo
+
+# sagetex
+*.sagetex.sage
+*.sagetex.py
+*.sagetex.scmd
+
+# sympy
+*.sout
+*.sympy
+sympy-plots-for-*.tex/
+
+# TikZ & PGF
+*.dpth
+*.md5
+*.auxlock
+
+# todonotes
+*.tdo
+
+# xindy
+*.xdy
+
+# WinEdt
+*.bak
+*.sav
+
+.DS_Store
+
+*.pyc
+
+__latexindent_temp.tex
+`
+
+const LICENSE = `The MIT License (MIT)
+
+Copyright (c) CURRENT_YEAR FULL_NAME
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+`
