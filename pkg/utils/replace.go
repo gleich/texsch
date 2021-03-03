8d5d4fa6c285573bf215f3ca8a8db37fb607ef55
Matthew Gleich <git@mattglei.ch>
2021-03-03 12:25:23 -0500

    🐛 Sort by length for map replace
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- pkg/utils/replace.go
+++ pkg/utils/replace.go
@@ -1,12 +1,23 @@
 package utils
 
-import "strings"
+import (
+	"sort"
+	"strings"
+)
 
 // Replace all keys in the original string with the given values of the map
 func ReplaceAllMapped(original string, toReplace map[string]string) string {
-	filledInDocument := original
-	for key, value := range toReplace {
-		filledInDocument = strings.ReplaceAll(filledInDocument, key, value)
+	var (
+		filledInDocument = original
+		keys             = MapKeys(toReplace)
+	)
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return len(keys[i]) > len(keys[j])
+	})
+
+	for _, key := range keys {
+		filledInDocument = strings.ReplaceAll(filledInDocument, key, toReplace[key])
 	}
 	return filledInDocument
 }
